025b6259d7d7f5199a140b12fc12391a3da24b77
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-05-23 21:23:54 -0300

    feat: add Login API

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use serde_json::json;
 
 //cmd: cargo watch -q -c -w examples/ -x 'run --example quick_dev'
 
@@ -6,11 +7,24 @@ use anyhow::Result;
 async fn main() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
+    //test Query Params
     hc.do_get("/hello?name=Jen").await?.print().await?;
 
+    //test Path Params
     hc.do_get("/hello2/Mike").await?.print().await?;
 
+    //test Static Route
     hc.do_get("/").await?.print().await?;
+
+    //request login
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username": "demo1",
+            "pwd":"welcome"
+        }),
+    );
+    req_login.await?.print().await?;
 
     Ok(())
 }
